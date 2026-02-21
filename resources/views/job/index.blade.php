b07e97a33dd0a980ad3bca2f8e6b772ff5ceef28
Mohamed-El-Barbary <mohamedelbarbary511@gmail.com>
2026-02-21 17:13:36 +0200

    Added Job Controller and index view

---
+++ resources/views/job/index.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <!-- Simplicity is the ultimate sophistication. - Leonardo da Vinci -->
+    <h1 class="text-3xl font-bold underline">
+        Hello world from Job Controller!
+    </h1>
+</div>
